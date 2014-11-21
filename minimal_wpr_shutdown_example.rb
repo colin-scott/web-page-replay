85305e46c20be8ab4a58354f05ab58b923d40616
colin-scott <cs@cs.berkeley.edu>
2014-11-20 17:32:07 -0800

    Wait for wpr to boot up

--- minimal_wpr_shutdown_example.rb
+++ minimal_wpr_shutdown_example.rb
@@ -4,9 +4,18 @@ require 'pty'
 
 raise 'Must run as root' unless Process.uid == 0
 
+# Spawn wpr:
 wpr_file = "/tmp/t.wpr"
 cmd = "./replay.py --record #{wpr_file}"
 (wpr_out, wpr_in, wpr_pid) = PTY.spawn cmd
+
+# Wait for wpr to boot up:
+wpr_out.each do |line|
+  puts "WPR: #{line}"
+  break if line =~ /HTTPS server started on/
+end
+
+# Kill wpr:
 [wpr_out, wpr_in].each { |stream| stream.close }
 Process.kill("TERM", wpr_pid)
 Process.wait wpr_pid
